77fa18a91a6de52635abd46747c0e35462ae364a
@duc0702 <ducnguyen07022004@gmail.com>
2024-07-01 14:12:57 +0700

    Buoi 1

---
+++ buoi1/buoi1.js
@@ -0,0 +1,18 @@
+// var test ='Test chữ hoa'
+// console.log(tAest)
+// console.log(Test)
+// var tuoi = 18
+// if(tuoi >17){
+//     console.log('Du tuoi di tu')
+// }
+// else{
+//     console.log('Van phai di trai')
+// }
+//  var ten= prompt('Xin chao SD19307')
+//  console.log(ten)
+var a = confirm('Nhập số thứ nhất:')
+console.error(a)
+// var b = prompt('Nhập số thứ hai:')
+
+
+// alert( a*1+b*1)
